b12e65d250ddff1cb16c783a689b2d50742934d0
adagrad <adagrad@protonmail.com>
2022-07-10 08:20:56 +0000

    add merge entry point

---
+++ entrypoint_merge.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+# show the command we intend executing
+echo merge $1 into main
+
+# prepare dolt environment
+echo dolt config
+echo "$DOLTHUB_SECRET_JWT" > /tmp/$DOLTHUB_SECRET.jwk
+dolt creds import /tmp/$DOLTHUB_SECRET.jwk
+
+dolt login $DOLTHUB_SECRET
+dolt config --list
+
+# clone repository
+dolt clone "$REPO/$DATABASE" && cd "$DATABASE"
+
+# merge, commit, push, delete
+dolt merge origin "$1"
+dolt commit -m"merged $1"
+dolt push
+dolt push origin --delete "$1"
