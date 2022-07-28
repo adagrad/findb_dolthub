fe28ae5ea5725fe67a91d24b04cba3a28b17bb25
adagrad <adagrad@protonmail.com>
2022-07-28 14:12:41 +0000

    fix utils

--- app/commands/dolt/push.py
+++ app/commands/dolt/push.py
@@ -10,10 +10,11 @@ if not hasattr(sys.modules[__name__], '__file__'):
 
 
 @click.command()
-@click.option('-a', '--stage-all', default=False, is_flag=True, help='Stage all changes')
+@click.option('-a', '--add-changes', type=str, default=None, help='Stage only specified changes seperated by blanks')
+@click.option('--stage-all', default=False, is_flag=True, help='Stage all changes')
 @click.option('-m', '--commit-message', type=str, default="merge", help='Commit message')
-def cli(stage_all, commit_message):
-    dolt_push(stage_all, commit_message)
+def cli(add_changes, stage_all, commit_message):
+    dolt_push(add_changes.split(" ") if add_changes is not None else stage_all, commit_message)
 
 
 if __name__ == '__main__':
--- app/modules/disk_utils.py
+++ app/modules/disk_utils.py
@@ -11,6 +11,9 @@ def check_disk_full(*paths, minimum="50MB"):
     bytes_size = humanfriendly.parse_size(minimum) if isinstance(minimum, str) else minimum
 
     for path in [os.getcwd(), *paths]:
+        if not os.path.exists(path):
+            continue
+
         total, used, free = shutil.disk_usage(path)
         totalh, usedh, freeh = humanfriendly.format_size(total), humanfriendly.format_size(used), humanfriendly.format_size(free)
         # print(f"total: {totalh}, used: {usedh}, free:{freeh}")
